0e554140fddbb1af4427da0dbbc39817c0173efc
hbybyyang <2451759073@qq.com>
2024-07-09 20:12:36 +0800

    引入task

--- src/plug/chinese-path.ts
+++ src/plug/chinese-path.ts
@@ -1,17 +1,22 @@
 import { URL } from 'node:url'
 import type { Request } from 'express'
 import { z } from 'zod'
+import { Task } from '@lsby/ts-fp-data'
 import { 插件 } from '../interface/plug'
 
 const zod类型表示 = z.object({})
 
 export class 中文路径支持插件 extends 插件<typeof zod类型表示> {
   constructor() {
-    super(zod类型表示, async (request: Request) => {
-      const url = new URL(request.url)
-      const { pathname } = url
-      request.url = request.originalUrl = pathname.replace(pathname, decodeURIComponent(pathname))
-      return {}
-    })
+    super(
+      zod类型表示,
+      (request: Request) =>
+        new Task(async () => {
+          const url = new URL(request.url)
+          const pathname = url.pathname
+          request.url = request.originalUrl = pathname.replace(pathname, decodeURIComponent(pathname))
+          return {}
+        }),
+    )
   }
 }
--- src/plug/json.ts
+++ src/plug/json.ts
@@ -1,30 +1,35 @@
 import { format } from 'node:util'
 import express from 'express'
 import type { z } from 'zod'
+import { Task } from '@lsby/ts-fp-data'
 import { GlobalLog } from '../global/global'
 import { 插件 } from '../interface/plug'
 
 export class JSON解析插件<Result extends z.ZodObject<{ body: z.AnyZodObject }>> extends 插件<Result> {
-  private log = GlobalLog.getInstance().extend('JsonParsePreApi')
+  private log = GlobalLog.getInstance().extend('JSON解析插件')
 
   constructor(t: Result, opt: Parameters<typeof express.json>[0]) {
-    super(t, async (req, res) => {
-      await new Promise((pRes, _rej) =>
-        express.json(opt)(req, res, () => {
-          pRes(null)
+    super(
+      t,
+      (req, res) =>
+        new Task(async () => {
+          await new Promise((pRes, _rej) =>
+            express.json(opt)(req, res, () => {
+              pRes(null)
+            }),
+          )
+
+          this.log.debug('准备解析 Json：%o', req.body)
+          const parseResult = t.safeParse({ body: req.body })
+
+          if (!parseResult.success) {
+            this.log.err('解析 Json 失败：%o', parseResult.error)
+            throw new Error(format('Parse JSON body failed: %O', parseResult.error))
+          }
+
+          this.log.debug('成功解析 Json：%o', parseResult.data.body)
+          return { body: parseResult.data.body }
         }),
-      )
-
-      this.log.debug('准备解析 Json：%o', req.body)
-      const parseResult = t.safeParse({ body: req.body })
-
-      if (!parseResult.success) {
-        this.log.err('解析 Json 失败：%o', parseResult.error)
-        throw new Error(format('Parse JSON body failed: %O', parseResult.error))
-      }
-
-      this.log.debug('成功解析 Json：%o', parseResult.data.body)
-      return { body: parseResult.data.body }
-    })
+    )
   }
 }
