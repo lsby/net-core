00b343f6843d039abfe5026c20ffa6487bcf91c6
hbybyyang <2451759073@qq.com>
2024-07-13 07:23:15 +0800

    修复无法启动的问题

--- src/server/server.ts
+++ src/server/server.ts
@@ -64,11 +64,12 @@ export class 服务器 {
       }
     })
 
-    var server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
+    var server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> | null = null
     await new Promise<void>((res, _rej) => {
       server = app.listen(this.端口, () => res())
     })
-    throw new Error('启动服务器失败')
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (server == null) throw new Error('启动服务器失败')
 
     return {
       ip: Object.values(networkInterfaces())
