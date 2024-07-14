c3a72e8e88467f8b901fba3fb3fd4f2c8dd3fc8b
hbybyyang <2451759073@qq.com>
2024-07-14 23:25:07 +0800

    插件使用task包装

--- src/interface/plug.ts
+++ src/interface/plug.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from 'express'
 import type { z } from 'zod'
+import { Task } from '@lsby/ts-fp-data'
 
 export class 插件<Obj extends z.AnyZodObject> {
-  protected declare readonly 类型保持符号?: Obj
+  protected declare readonly __类型保持符号?: Obj
 
   constructor(
     private 类型: Obj,
@@ -19,15 +20,15 @@ export class 插件<Obj extends z.AnyZodObject> {
 }
 
 export type 任意插件 = 插件<any>
-export type 插件项类型 = 插件<z.AnyZodObject>
-export type 取插件内部类型<A> = A extends 插件<infer x> ? x : never
+export type 插件项类型 = Task<插件<z.AnyZodObject>>
+export type 取插件内部类型<A> = A extends Task<插件<infer x>> ? x : never
 
 export type 合并插件结果<Arr extends Array<插件项类型>> = Arr extends []
   ? {}
   : Arr extends [infer x, ...infer xs]
-    ? x extends infer 插件
+    ? x extends infer 插件项
       ? xs extends Array<插件项类型>
-        ? z.infer<取插件内部类型<插件>> & 合并插件结果<xs>
+        ? z.infer<取插件内部类型<插件项>> & 合并插件结果<xs>
         : {}
       : {}
     : {}
