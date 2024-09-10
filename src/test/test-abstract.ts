f7c8a1c97dce1d133fe7aecbb9d8ea2a21f2c105
hbybyyang <2451759073@qq.com>
2024-09-11 02:51:22 +0800

    解决测试依赖关系

--- src/test/test-abstract.ts
+++ src/test/test-abstract.ts
@@ -1,14 +1,12 @@
-import { 测试 } from './test'
-
-export abstract class 测试抽象类 extends 测试 {
-  constructor() {
-    super(null as any, null as any, null as any, null as any)
-
-    this.前置 = this.前置实现
-    this.中置 = this.中置实现
-    this.后置 = this.后置实现
-  }
+export abstract class 测试抽象类 {
+  constructor() {}
   abstract 前置实现(): Promise<void>
   abstract 中置实现(): Promise<object>
   abstract 后置实现(中置结果: object): Promise<void>
+
+  async 运行(): Promise<void> {
+    await this.前置实现()
+    var 中置结果 = await this.中置实现()
+    await this.后置实现(中置结果)
+  }
 }
