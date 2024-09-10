b60fd5f7dc3ad2f660e8508c5532aa5851a70171
hbybyyang <2451759073@qq.com>
2024-09-11 04:46:03 +0800

    接口类型允许使用抽象类构造

--- src/bin/gen-type/calc-code.ts
+++ src/bin/gen-type/calc-code.ts
@@ -1,10 +1,10 @@
 export const calcCode = `
 import type { z } from 'zod'
-import type { 合并JSON插件结果, 接口类型 } from '@lsby/net-core'
+import type { 合并JSON插件结果, 接口类型抽象类 } from '@lsby/net-core'
 import exportedApiSchema from './type'
 
 type Api =
-  typeof exportedApiSchema extends 接口类型<
+  typeof exportedApiSchema extends 接口类型抽象类<
     infer Path,
     infer Method,
     infer PreApis,
