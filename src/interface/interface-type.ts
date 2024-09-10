a7c79471c0e7d8f49f62934188a055d6e10fd173
hbybyyang <2451759073@qq.com>
2024-09-11 03:41:16 +0800

    Revert "接口类型允许使用抽象类构造"
    This reverts commit 5c9fe07c0122c1138074fa3ffee10d1ef409b9aa.

--- src/interface/interface-type.ts
+++ src/interface/interface-type.ts
@@ -1,6 +1,5 @@
 import type { z } from 'zod'
-import { 插件项类型 } from '../plug'
-import { 接口类型抽象类 } from './interface-type-abstract'
+import { 插件项类型 } from './plug'
 
 export class 接口类型<
   路径 extends string,
@@ -8,30 +7,37 @@ export class 接口类型<
   插件们 extends Array<插件项类型>,
   正确结果类型 extends z.ZodTypeAny,
   错误结果类型 extends z.ZodTypeAny,
-> extends 接口类型抽象类<路径, 方法, 插件们, 正确结果类型, 错误结果类型> {
+> {
   protected declare readonly __类型保持符号?: [路径, 方法, 插件们, 正确结果类型, 错误结果类型]
+
   constructor(
     private 路径: 路径,
     private 方法: 方法,
     private 插件们: [...插件们],
     private 正确结果: 正确结果类型,
     private 错误结果: 错误结果类型,
-  ) {
-    super()
-  }
-  override 获得路径(): 路径 {
+  ) {}
+
+  获得路径(): 路径 {
     return this.路径
   }
-  override 获得方法(): 方法 {
+
+  获得方法(): 方法 {
     return this.方法
   }
-  override 获得插件们(): [...插件们] {
+
+  获得插件们(): [...插件们] {
     return this.插件们
   }
-  override 获得正确结果类型(): 正确结果类型 {
+
+  获得正确结果类型(): 正确结果类型 {
     return this.正确结果
   }
-  override 获得错误结果类型(): 错误结果类型 {
+
+  获得错误结果类型(): 错误结果类型 {
     return this.错误结果
   }
 }
+
+export type 任意接口类型 = 接口类型<any, any, any, any, any>
+export type 获得接口插件们<接口类型描述> = 接口类型描述 extends 接口类型<any, any, infer 插件, any, any> ? 插件 : never
--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -1,8 +1,8 @@
 import type { z } from 'zod'
-import type { 正确结果, 错误结果 } from '../../result/result'
-import { 合并插件结果, 插件项类型 } from '../plug'
-import { 接口类型抽象类 } from '../type/interface-type-abstract'
+import type { 正确结果, 错误结果 } from '../result/result'
 import { 接口抽象类 } from './interface-abstract'
+import { 接口类型 } from './interface-type'
+import { 合并插件结果, 插件项类型 } from './plug'
 
 export class 接口<
   路径 extends string,
@@ -10,10 +10,10 @@ export class 接口<
   插件们 extends Array<插件项类型>,
   正确返回类型 extends z.ZodTypeAny,
   错误返回类型 extends z.ZodTypeAny,
-> extends 接口抽象类<接口类型抽象类<路径, 方法, 插件们, 正确返回类型, 错误返回类型>> {
+> extends 接口抽象类<接口类型<路径, 方法, 插件们, 正确返回类型, 错误返回类型>> {
   protected declare readonly __类型保持符号?: [路径, 方法, 插件们, 正确返回类型, 错误返回类型]
   constructor(
-    private 接口类型: 接口类型抽象类<路径, 方法, 插件们, 正确返回类型, 错误返回类型>,
+    private 接口类型: 接口类型<路径, 方法, 插件们, 正确返回类型, 错误返回类型>,
     private 实现: (
       插件结果: 合并插件结果<插件们>,
     ) => Promise<正确结果<z.infer<正确返回类型>> | 错误结果<z.infer<错误返回类型>>>,
@@ -21,7 +21,7 @@ export class 接口<
     super()
   }
 
-  override 获得类型(): 接口类型抽象类<路径, 方法, 插件们, 正确返回类型, 错误返回类型> {
+  override 获得类型(): 接口类型<路径, 方法, 插件们, 正确返回类型, 错误返回类型> {
     return this.接口类型
   }
 
