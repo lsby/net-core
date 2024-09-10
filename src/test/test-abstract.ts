d0c1059668df401b2b8fee5cfdb03323c8054709
hbybyyang <2451759073@qq.com>
2024-09-11 02:13:31 +0800

    允许测试使用继承类的方式实现

--- src/test/test-abstract.ts
+++ src/test/test-abstract.ts
@@ -1,11 +1,14 @@
 import { 测试 } from './test'
 
 export abstract class 测试抽象类 extends 测试 {
-  abstract override 前置: () => Promise<void>
-  abstract override 中置: () => Promise<object>
-  abstract override 后置: (中置结果: object) => Promise<void>
-
   constructor() {
     super(null as any, null as any, null as any, null as any)
+
+    this.前置 = this.前置实现
+    this.中置 = this.中置实现
+    this.后置 = this.后置实现
   }
+  abstract 前置实现(): Promise<void>
+  abstract 中置实现(): Promise<object>
+  abstract 后置实现(中置结果: object): Promise<void>
 }
