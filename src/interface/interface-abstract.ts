7e32a0b2ffde95887d8eb7293bc8aa7605816cad
hbybyyang <2451759073@qq.com>
2024-09-11 03:00:45 +0800

    解决接口依赖关系

--- src/interface/interface-abstract.ts
+++ src/interface/interface-abstract.ts
@@ -1,11 +1,10 @@
 import { z } from 'zod'
 import { 正确结果, 错误结果 } from '../result/result'
-import { 接口 } from './interface'
-import { 接口类型 } from './interface-type'
+import { 任意接口类型, 接口类型 } from './interface-type'
 import { 合并插件结果 } from './plug'
 
-type F1<A> = A extends 接口类型<infer A1, infer _A2, infer _A3, infer _A4, infer _A5> ? A1 : never
-type F2<A> = A extends 接口类型<infer _A1, infer A2, infer _A3, infer _A4, infer _A5> ? A2 : never
+type _F1<A> = A extends 接口类型<infer A1, infer _A2, infer _A3, infer _A4, infer _A5> ? A1 : never
+type _F2<A> = A extends 接口类型<infer _A1, infer A2, infer _A3, infer _A4, infer _A5> ? A2 : never
 type F3<A> = A extends 接口类型<infer _A1, infer _A2, infer A3, infer _A4, infer _A5> ? A3 : never
 type F4<A> = A extends 接口类型<infer _A1, infer _A2, infer _A3, infer A4, infer _A5> ? A4 : never
 type F5<A> = A extends 接口类型<infer _A1, infer _A2, infer _A3, infer _A4, infer A5> ? A5 : never
@@ -14,26 +13,11 @@ export type 计算实现结果<接口类型定义> = Promise<
   正确结果<z.infer<F4<接口类型定义>>> | 错误结果<z.infer<F5<接口类型定义>>>
 >
 
-export abstract class 接口抽象类<接口类型描述 extends 接口类型<any, any, any, any, any>> extends 接口<
-  F1<接口类型描述>,
-  F2<接口类型描述>,
-  F3<接口类型描述>,
-  F4<接口类型描述>,
-  F5<接口类型描述>
-> {
-  constructor() {
-    super(null as any, null as any)
-  }
-
-  abstract override 获得类型(): 接口类型描述
-
-  override 获得实现(): (
-    ctx: object,
-  ) => Promise<正确结果<z.infer<F4<接口类型描述>>> | 错误结果<z.infer<F5<接口类型描述>>>> {
-    return this.调用 as any
-  }
-
+export abstract class 接口抽象类<接口类型描述 extends 任意接口类型> {
+  abstract 获得类型(): 接口类型描述
   abstract 调用(
     ctx: 合并插件结果<F3<接口类型描述>>,
   ): Promise<正确结果<z.infer<F4<接口类型描述>>> | 错误结果<z.infer<F5<接口类型描述>>>>
 }
+
+export type 任意接口 = 接口抽象类<any>
