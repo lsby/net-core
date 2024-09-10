14a9c3b1a20545ed51cb4793f4e6ca6c19f469a5
hbybyyang <2451759073@qq.com>
2024-09-11 02:51:22 +0800

    解决测试依赖关系

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,16 +1,23 @@
 import { 任意接口类型 } from '../interface/interface-type'
+import { 测试抽象类 } from './test-abstract'
 
-export class 测试 {
+export class 测试 extends 测试抽象类 {
   constructor(
     private 接口类型: 任意接口类型,
     protected 前置: () => Promise<void>,
     protected 中置: () => Promise<object>,
     protected 后置: (中置结果: object) => Promise<void>,
-  ) {}
+  ) {
+    super()
+  }
 
-  async 运行(): Promise<void> {
-    await this.前置()
-    var 中置结果 = await this.中置()
-    await this.后置(中置结果)
+  override 前置实现(): Promise<void> {
+    return this.前置()
+  }
+  override 中置实现(): Promise<object> {
+    return this.中置()
+  }
+  override 后置实现(中置结果: object): Promise<void> {
+    return this.后置(中置结果)
   }
 }
