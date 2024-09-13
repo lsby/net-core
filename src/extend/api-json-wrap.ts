cc7f4c9793e8826d491e231d6930f22548b8366d
hbybyyang <2451759073@qq.com>
2024-09-14 05:00:19 +0800

    优化接口模型

--- src/extend/api-json-wrap.ts
+++ src/extend/api-json-wrap.ts
@@ -1,8 +1,13 @@
 import { z } from 'zod'
-import { Either, Left, Right } from '@lsby/ts-fp-data'
-import { 插件项类型 } from '../interface/plug'
-import { 接口类型抽象类, 接口类型正确结果, 接口类型错误结果 } from '../interface/type/interface-type-abstract'
-import { JSON接口基类 } from './api-json-base'
+import { API接口基类 } from '../interface/inst/interface-abstract'
+import { 合并插件结果, 插件项类型 } from '../interface/plug'
+import {
+  接口类型抽象类,
+  接口类型插件们,
+  接口类型正确结果,
+  接口类型错误结果,
+} from '../interface/type/interface-type-abstract'
+import { 正确JSON结果, 正确结果, 错误JSON结果, 错误结果 } from '../result/result'
 
 export abstract class 包装的接口类型抽象类<
   路径 extends string,
@@ -30,8 +35,6 @@ export abstract class 包装的接口类型抽象类<
   abstract override 获得错误结果类型(): z.ZodObject<{ status: z.ZodLiteral<'fail'>; data: 错误返回类型 }>
 }
 type 任意的包装的接口类型 = 包装的接口类型抽象类<any, any, any, any, any>
-type 取返回类型<A> = A extends 包装的接口类型抽象类<any, any, any, infer X, any> ? X : never
-type 取错误类型<A> = A extends 包装的接口类型抽象类<any, any, any, any, infer X> ? X : never
 
 export class 包装的接口类型<
   路径 extends string,
@@ -67,21 +70,12 @@ export class 包装的接口类型<
   }
 }
 
-export abstract class JSON接口包装基类<接口类型描述 extends 任意的包装的接口类型> extends JSON接口基类<接口类型描述> {
-  protected 构造正确返回(
-    data: z.infer<取返回类型<接口类型描述>>,
-  ): Either<z.infer<接口类型错误结果<接口类型描述>>, z.infer<接口类型正确结果<接口类型描述>>> {
-    return new Right({ status: 'success' as const, data })
-  }
-  protected 构造错误返回(
-    data: z.infer<取错误类型<接口类型描述>>,
-  ): Either<z.infer<接口类型错误结果<接口类型描述>>, z.infer<接口类型正确结果<接口类型描述>>> {
-    return new Left({ status: 'fail' as const, data })
-  }
-  protected 构造包装返回(
-    data: Either<z.infer<取错误类型<接口类型描述>>, z.infer<取返回类型<接口类型描述>>>,
-  ): Either<z.infer<接口类型错误结果<接口类型描述>>, z.infer<接口类型正确结果<接口类型描述>>> {
-    if (data.isLeft()) return new Left({ status: 'fail' as const, data: data.assertLeft().getLeft() })
-    return new Right({ status: 'success' as const, data: data.assertRight().getRight() })
+export abstract class JSON接口包装基类<接口类型描述 extends 任意的包装的接口类型> extends API接口基类<接口类型描述> {
+  override async API实现(
+    参数: 合并插件结果<接口类型插件们<接口类型描述>>,
+  ): Promise<正确结果<z.infer<接口类型正确结果<接口类型描述>>> | 错误结果<z.infer<接口类型错误结果<接口类型描述>>>> {
+    var c = await this.业务行为实现(参数)
+    if (c.isLeft()) return new 错误JSON结果({ status: 'fail' as const, data: c.assertLeft().getLeft() })
+    return new 正确JSON结果({ status: 'success' as const, data: c.assertRight().getRight() })
   }
 }
