42f7da15011ea3af8586f6a541d5fd0114babed8
hbybyyang <2451759073@qq.com>
2024-09-14 05:09:56 +0800

    修复JSON接口包装基类

--- src/extend/api-json-wrap.ts
+++ src/extend/api-json-wrap.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'
-import { API接口基类 } from '../interface/inst/interface-abstract'
+import { Either } from '@lsby/ts-fp-data'
+import { API接口基类, 计算实现参数 } from '../interface/inst/interface-abstract'
 import { 合并插件结果, 插件项类型 } from '../interface/plug'
 import {
   接口类型抽象类,
@@ -71,6 +72,9 @@ export class 包装的接口类型<
 }
 
 export abstract class JSON接口包装基类<接口类型描述 extends 任意的包装的接口类型> extends API接口基类<接口类型描述> {
+  protected abstract override 业务行为实现(
+    参数: 计算实现参数<接口类型描述>,
+  ): Promise<Either<z.infer<接口类型错误结果<接口类型描述>>['data'], z.infer<接口类型正确结果<接口类型描述>>['data']>>
   override async API实现(
     参数: 合并插件结果<接口类型插件们<接口类型描述>>,
   ): Promise<正确结果<z.infer<接口类型正确结果<接口类型描述>>> | 错误结果<z.infer<接口类型错误结果<接口类型描述>>>> {
