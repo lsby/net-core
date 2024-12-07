666e30b64df0207524b3b10acf6a756aeb6b37a5
hbybyyang <2451759073@qq.com>
2024-12-08 04:11:02 +0800

    调整日志

--- src/plugin/extend/custom-data.ts
+++ src/plugin/extend/custom-data.ts
@@ -1,8 +1,15 @@
 import type { z } from 'zod'
+import { Global } from '../../global/global'
 import { 插件 } from '../plug'
 
 export class 自定义数据插件<Data extends z.AnyZodObject> extends 插件<Data> {
+  private log = Global.getItem('log')
+
   constructor(t: Data, data: z.infer<Data>) {
-    super(t, async () => data)
+    super(t, async (_res, _req, 附加参数) => {
+      let log = (await this.log).extend(附加参数.请求id).extend('自定义数据插件')
+      await log.debug('自定义数据插件运行, 结果: %o', data)
+      return data
+    })
   }
 }
