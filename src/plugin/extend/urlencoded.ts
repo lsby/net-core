fa7297e5c22b181f36d91176e4e18854a6d5af87
hbybyyang <2451759073@qq.com>
2024-12-08 03:44:18 +0800

    统一请求id和日志

--- src/plugin/extend/urlencoded.ts
+++ src/plugin/extend/urlencoded.ts
@@ -1,11 +1,16 @@
 import { format } from 'node:util'
 import express from 'express'
 import type { z } from 'zod'
+import { Global } from '../../global/global'
 import { 插件 } from '../plug'
 
 export class 表单解析插件<Result extends z.AnyZodObject> extends 插件<Result> {
+  private log = Global.getItem('log')
+
   constructor(t: Result, opt: Parameters<typeof express.urlencoded>[0]) {
-    super(t, async (req, res) => {
+    super(t, async (req, res, 附加参数) => {
+      let log = (await this.log).extend(附加参数.请求id).extend('JSON解析插件')
+
       await new Promise((pRes, _rej) =>
         express.urlencoded({ extended: true, ...opt })(req, res, () => {
           pRes(null)
@@ -13,7 +18,10 @@ export class 表单解析插件<Result extends z.AnyZodObject> extends 插件<Re
       )
 
       let parseResult = t.safeParse(req.body)
-      if (!parseResult.success) throw new Error(format('解析url编码正文失败: %O', parseResult.error))
+      if (!parseResult.success) {
+        await log.err('解析url编码正文失败: %O', parseResult.error)
+        throw new Error(format('解析url编码正文失败: %O', parseResult.error))
+      }
 
       return parseResult.data
     })
