7f22b21875ae12bfd33b418665927c6ce65d45b1
hbybyyang <2451759073@qq.com>
2025-04-11 06:18:42 +0800

    支持类型别名

--- src/bin/gen-api-type/test.ts
+++ src/bin/gen-api-type/test.ts
@@ -1,7 +1,7 @@
 // import { main } from '.'
 
 // await main(
-//   'D:\\Code\\playground-ts-service\\tsconfig.json',
-//   'D:\\Code\\playground-ts-service\\src\\interface',
-//   'D:\\Code\\playground-ts-service\\src\\types\\interface-type.ts',
+//   'D:\\Code\\yos-20241010\\tsconfig.json',
+//   'D:\\Code\\yos-20241010\\src\\interface',
+//   'D:\\Code\\yos-20241010\\src\\types\\interface-type.ts',
 // )
