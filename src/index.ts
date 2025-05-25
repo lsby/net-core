4a80dc1cc352ed6b7086f7301e973d5fdcb87836
hbybyyang <2451759073@qq.com>
2025-05-26 02:00:53 +0800

    整理

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export * from './action/action.bak'
 
-export * from './interface-api/interface-base'
-export * from './interface-api/interface-logic'
-export * from './interface-api/interface-result'
-export * from './interface-api/interface-test'
+export * from './interface/interface-base'
+export * from './interface/interface-logic'
+export * from './interface/interface-result'
+export * from './interface/interface-test'
 
 export * from './plugin/extend/chinese-path'
 export * from './plugin/extend/custom-data'
