462f79249aa8e025ad1cb43e98bc4fe90d358b16
hbybyyang <2451759073@qq.com>
2025-09-10 03:45:57 +0800

    修复ws时序

--- src/global/global.ts
+++ src/global/global.ts
@@ -1,12 +1,8 @@
-import { GlobalAsyncItem, GlobalService } from '@lsby/ts-global'
+import { GlobalItem, GlobalService } from '@lsby/ts-global'
 import { Log } from '@lsby/ts-log'
 import { WebSocket管理器 } from './web-socket-management'
 
 export let Global = new GlobalService([
-  new GlobalAsyncItem('log', async () => {
-    return new Log('@lsby:net-core')
-  }),
-  new GlobalAsyncItem('WebSocket管理器', async () => {
-    return new WebSocket管理器({})
-  }),
+  new GlobalItem('log', new Log('@lsby:net-core')),
+  new GlobalItem('WebSocket管理器', new WebSocket管理器({})),
 ])
--- src/global/web-socket-management.ts
+++ src/global/web-socket-management.ts
@@ -4,15 +4,15 @@ import { Global } from './global'
 type id = string
 
 export class WebSocket管理器 {
-  private log = Global.getItem('log').then((a) => a.extend('@lsby:net-core').extend('WebSocket管理器'))
+  private log = Global.getItemSync('log').extend('@lsby:net-core').extend('WebSocket管理器')
   private 清理函数表: Record<string, () => Promise<void>> = {}
 
   public constructor(private 连接表: Record<id, WebSocket | null>) {}
 
-  public async 增加连接(id: id, ws句柄: WebSocket): Promise<void> {
+  public 增加连接(id: id, ws句柄: WebSocket): void {
     this.连接表[id] = ws句柄
   }
-  public async 查询连接存在(id: string): Promise<boolean> {
+  public 查询连接存在(id: string): boolean {
     return this.连接表.hasOwnProperty(id) ? true : false
   }
   public async 设置清理函数(id: string, 清理函数: () => Promise<void>): Promise<void> {
@@ -22,21 +22,16 @@ export class WebSocket管理器 {
   public async 获得句柄(id: id): Promise<WebSocket | null> {
     return this.连接表[id] ?? null
   }
-  public async 删除连接(id: string): Promise<void> {
-    let log = await this.log
+  public 删除连接(id: string): void {
     let 清理函数 = this.清理函数表[id]
 
     try {
       this.连接表[id]?.close(1000, '服务器主动关闭')
     } catch (err) {
-      await log.error(`关闭 WebSocket 句柄失败, id: ${id}, 错误: ${err}`)
+      this.log.errorSync(`关闭 WebSocket 句柄失败, id: ${id}, 错误: ${err}`)
     }
 
-    try {
-      await 清理函数?.()
-    } catch (err) {
-      await log.error(`清理连接失败, id: ${id}, 错误: ${err}`)
-    }
+    清理函数?.().catch((err) => this.log.errorSync(`清理连接失败, id: ${id}, 错误: ${err}`))
 
     delete this.连接表[id]
     delete this.清理函数表[id]
