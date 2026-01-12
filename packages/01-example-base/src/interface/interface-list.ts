87005de456eb0e1a0a4309f387accad2c70908c6
lsby <2451759073@qq.com>
2026-01-12 10:30:27 +0800

    整理

--- packages/01-example-base/src/interface/interface-list.ts
+++ packages/01-example-base/src/interface/interface-list.ts
@@ -4,17 +4,17 @@ import { 任意接口 } from '@lsby/net-core'
 import _src_interface_01_calculate_add_index_ts from './01-calculate-add/index'
 import _src_interface_02_calculate_sub_index_ts from './02-calculate-sub/index'
 import _src_interface_03_pure_string_index_ts from './03-pure-string/index'
-import _src_interface_04_form_submit_index_ts from './04-form-submit/index'
 import _src_interface_05_download_file_index_ts from './05-download-file/index'
 import _src_interface_06_static_file_index_ts from './06-static-file/index'
 import _src_interface_07_virtual_file_index_ts from './07-virtual-file/index'
+import _src_interface_04_url_encoded_submit_index_ts from './04-url-encoded-submit/index'
 
 export let interfaceApiList: 任意接口[] = [
   _src_interface_01_calculate_add_index_ts,
   _src_interface_02_calculate_sub_index_ts,
   _src_interface_03_pure_string_index_ts,
-  _src_interface_04_form_submit_index_ts,
   _src_interface_05_download_file_index_ts,
   _src_interface_06_static_file_index_ts,
   _src_interface_07_virtual_file_index_ts,
+  _src_interface_04_url_encoded_submit_index_ts,
 ]
