de87174f0244c18f7e2246db5ad5fc4bdbe36a4b
lsby <2451759073@qq.com>
2026-01-12 18:45:24 +0800

    更新到eslint9

--- packages/02-example-plugins/src/interface/interface-list.ts
+++ packages/02-example-plugins/src/interface/interface-list.ts
@@ -5,16 +5,16 @@ import _src_interface_01_json_plugin_index_ts from './01-json-plugin/index'
 import _src_interface_02_query_plugin_index_ts from './02-query-plugin/index'
 import _src_interface_03_urlencoded_plugin_index_ts from './03-urlencoded-plugin/index'
 import _src_interface_04_custom_data_plugin_index_ts from './04-custom-data-plugin/index'
-import _src_interface_06_multiple_plugins_index_ts from './05-multiple-plugins/index'
-import _src_interface_07_make_plugin_index_ts from './06-make-plugin/index'
-import _src_interface_05_custom_plugin_index_ts from './07-custom-plugin/index'
+import _src_interface_05_multiple_plugins_index_ts from './05-multiple-plugins/index'
+import _src_interface_06_make_plugin_index_ts from './06-make-plugin/index'
+import _src_interface_07_custom_plugin_index_ts from './07-custom-plugin/index'
 
 export let interfaceApiList: 任意接口[] = [
   _src_interface_01_json_plugin_index_ts,
-  _src_interface_04_custom_data_plugin_index_ts,
   _src_interface_02_query_plugin_index_ts,
   _src_interface_03_urlencoded_plugin_index_ts,
-  _src_interface_05_custom_plugin_index_ts,
-  _src_interface_06_multiple_plugins_index_ts,
-  _src_interface_07_make_plugin_index_ts,
+  _src_interface_04_custom_data_plugin_index_ts,
+  _src_interface_05_multiple_plugins_index_ts,
+  _src_interface_06_make_plugin_index_ts,
+  _src_interface_07_custom_plugin_index_ts,
 ]
