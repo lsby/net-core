282a56125e263c3ff38f0a838dc7e95d29e35378
lsby <2451759073@qq.com>
2026-07-28 02:25:55 +0800

    Revert "适配前端引用"
    This reverts commit add48b2fe8f218fe9965455362693e5596af7c7b.

--- packages/core/src/browser-mock/multer.ts
+++
@@ -1,6 +0,0 @@
-let dummyMiddleware =
-  () =>
-  (req: any, res: any, next: any): any =>
-    next()
-
-export default (): { any: () => (req: any, res: any, next: any) => any } => ({ any: dummyMiddleware })
